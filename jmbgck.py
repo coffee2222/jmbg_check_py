33d83d1610831ed7968f88e29f84e68a0f434d31
coffee2222 <79166058+coffee2222@users.noreply.github.com>
2021-02-16 20:55:51 +0100

    Update jmbgck.py

--- jmbgck.py
+++ jmbgck.py
@@ -54,10 +54,69 @@ def dob(): # first 7 numbers from jmbg.
 
     try:
         newDate = datetime.datetime(int(year),int(month),int(date))
-        print(colored("DOB:" + str(date) + "d-" + str(month) + "m-"+ str(year)+ "y" , 'cyan'))
-    except ValueError:
-       print(colored("DOB ERROR:" + str(date) + "d-" + str(month) + "m-"+ str(year)+ "y", 'red'))
+import sys
+import time
+import argparse as ap
 
+import datetime
+from termcolor import colored
+def main():
+    try:
+        jmbg = (str(sys.argv[1]))
+    except:
+        sys.exit("JMBG not provided!")
+    if (len(jmbg) == 13):
+        print(jmbg)
+        print("JMBG number is correct length!")
+       
+        print(dob())
+        print(location(str(list(jmbg)[7] + list(jmbg)[8])))
+        print(mof())
+        controlnumber()
+      
+
+    else:
+        print("Not a valid JMBG number")
+
+
+def controlnumber():
+    jmbg = (str(sys.argv[1]))
+    L = 11 - ((7 *( (int(list(jmbg)[0])) + (int(list(jmbg)[6])) ) + 6 * ( (int(list(jmbg)[1])) + (int(list(jmbg)[7])) ) + 5 * ( (int(list(jmbg)[2])) + (int(list(jmbg)[8])) ) + 4 * ( (int(list(jmbg)[3])) + (int(list(jmbg)[9])) )  + 3*  ( (int(list(jmbg)[4])) + (int(list(jmbg)[10])) ) + 2* ( (int(list(jmbg)[5])) + (int(list(jmbg)[11])) ) ))
+    if int(list(jmbg)[12]) == (L%11):
+         print(colored('JMBG control number valid!', 'green'))
+         return
+    elif int(list(jmbg)[12]) != (L%11):
+        print(colored('JMBG control number invalid!', 'red'))
+        return
+def mof():
+    jmbg = (str(sys.argv[1]))
+    mofnum=  str(list(jmbg)[9] + list(jmbg)[10] +  list(jmbg)[11])
+    if  500  <= int(mofnum) <= 999:
+        return 'Female'
+    elif 0 <= int(mofnum) <= 499:
+        return 'Male'
+def dob(): # first 7 numbers from jmbg.
+    jmbg = (str(sys.argv[1]))
+    date = str(list(jmbg)[0] + list(jmbg)[1])
+    month = str(list(jmbg)[2] + list(jmbg)[3])
+    y = str(list(jmbg)[4] + list(jmbg)[5] + list(jmbg)[6]) 
+    CurrentDate = datetime.datetime.now()
+    if 900 <int(y) < 1000:
+        year = "1" + str(list(jmbg)[4] + list(jmbg)[5] + list(jmbg)[6]) 
+    elif int(y) > 0 :
+        year = "2" + str(list(jmbg)[4] + list(jmbg)[5] + list(jmbg)[6])
+    elif int(y) == 0:
+        year = "2000"
+    ExpectedDate = date +"/"+month + "/" + year + " 4:00"
+    try:
+        newDate = datetime.datetime(int(year),int(month),int(date))
+        ExpectedDate = datetime.datetime.strptime(ExpectedDate, "%d/%m/%Y %H:%M")
+        if (CurrentDate < ExpectedDate):
+            raise ValueError
+        return (colored("DOB:" + str(date) + "d-" + str(month) + "m-"+ str(year)+ "y" , 'cyan')) 
+    except ValueError as e:
+        return (colored("DOB ERROR:" + str(date) + "d-" + str(month) + "m-"+ str(year)+ "y", 'red'))
+        
          
 def location(val):
     return {
@@ -111,6 +170,65 @@ def location(val):
         '70' : "Serbia : Central Serbia" ,
         '71':"Serbia : Central Serbia : Beograd region ",
         '72': "Serbia : Central Serbia : Sumadija",
+        '73': "Serbia : Central Serbia : Nis",
+        '74': "Serbia : Central Serbia : Juzna Morava",
+        '75':"Serbia : Central Serbia : Zajecar",
+        '76': "Serbia : Central Serbia : Podunavlje",
+        '77': "Serbia : Central Serbia : Podrinje i Kolubara",
+        '78': "Serbia : Central Serbia : Kraljevo",
+        '79': "Serbia : Central Serbia : Uzice ",
+        '80': "Serbia : Autonomous Province of Vojvodina : Novi Sad ",
+        '81': "Serbia : Autonomous Province of Vojvodina : Sombor",
+        '82': "Serbia : Autonomous Province of Vojvodina : Subotica",
+        '84' or '83': "Serbia : Autonomous Province of Vojvodina",
+        '85': "Serbia : Autonomous Province of Vojvodina : Zrenjanin",
+        '86': "Serbia : Autonomous Province of Vojvodina : Pancevo",
+        '87': "Serbia : Autonomous Province of Vojvodina : Kikinda",
+        '88': "Serbia : Autonomous Province of Vojvodina : Ruma",
+        '89': "Serbia : Autonomous Province of Vojvodina : Sremska Mitrovica",
+        
+        '91': "Serbia : Autonomous Province of Kosovo and Metohija  : Pristina",
+        '92': "Serbia : Autonomous Province of Kosovo and Metohija  : Kosovska Mitrovica",
+        '93': "Serbia : Autonomous Province of Kosovo and Metohija  : Pec",
+        '94': "Serbia : Autonomous Province of Kosovo and Metohija  : Djakovica",
+        '95': "Serbia : Autonomous Province of Kosovo and Metohija  : Prizren",
+        '96': "Serbia : Autonomous Province of Kosovo and Metohija  : Kosovo Pomoravski District",
+        '97' or '98' or '99': "Serbia : Autonomous Province of Kosovo and Metohija",
+    }.get(val, 'Not used!') 
+
+if __name__ == "__main__":
+    print('Developed by (offe^4')
+    print('version 0.0.1')
+    print(colored(":----------:::::://////::::////:::::::-----------:", 'white',))
+    print(colored(":.-.-:..------:--:///::::/:-://///////:::/::-----:", 'white',))
+    print(colored(":.-.---.------....//////-///://-::::::/+++:---..-:", 'white',))
+    print(colored(":...`........```.::::----............-/+/:------.:", 'white',))
+    print(colored(":.`  `-/o+/-`  `./+//-.............-//::-----..--:", 'white',))
+    print(colored(":.`  /hydhyho` `.////::::........-//:--++++++-...:", 'white',))
+    print(colored(":.` `so///+hd. `.+///+///-----...-:--..////+/----:", 'white',))
+    print(colored(":.`  /so//o+s. `./+://:///+-...........://///--:-:", 'white',))
+    print(colored(":.   .+++/:/-` `.//-:-----///::/::::--...--------:", 'white',))
+    print(colored(":.  `.+oo+//:.``.+/--::::::////::::::---.-//.---.:", 'white',))
+    print(colored(":`..-:oo+///o::-.+/:///++/+-..-..--.----.-//.....:", 'white',))
+    print(colored(":...--+/::::+--:.//://////-..........---.:::-....:", 'white',))
+    print(colored(":..------:-------/:-:::/::/.........------.......:", 'white',))
+    print(colored(":.:++:++/+/+o/+++/////-----......----://::-/::...:", 'white',))
+    print(colored(":----------::--:::::::-------::------------------:", 'white',))
+    print(colored('Jedinstveni matični broj građana', 'white', 'on_blue'))
+    print(colored('exYu ID number check', 'red', 'on_white'))
+    print(colored('_____________________________________________________________________________________________________________________','red'))
+    print('Keep in mind: This tools just check the data based on set of rules. It does not communicate to any external database!')
+    print(colored('_____________________________________________________________________________________________________________________','red'))
+    time.sleep(2)
+    print()
+    print()
+    print()
+    main()
+    print(colored('_____________________________________________________________________________________________________________________','red'))
+
+
+
+
         '73': "Serbia : Central Serbia : Nis",
         '74': "Serbia : Central Serbia : Juzna Morava",
         '75':"Serbia : Central Serbia : Zajecar",
